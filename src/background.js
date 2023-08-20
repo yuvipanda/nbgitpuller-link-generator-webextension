aee8d5e4f1324a4a3886aa09779f28f6ac9a4360
Pamela Fox <pamela.fox@gmail.com>
2023-08-20 13:08:08 -0700

    Update src/background.js
    Co-authored-by: Yuvi Panda <yuvipanda@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log(tab.url);
     if (tab.url.startsWith("https://github.com/")) {
         chrome.action.setIcon({ tabId, path: "icon_32.png" });
         chrome.action.setPopup({ tabId, popup: "popup.html" });
